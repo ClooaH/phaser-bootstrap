18ad3d2ce884e921e21e9dd15ca758908c1ded75
JensAndreasson <jensandreasson77@gmail.com>
2020-02-05 21:02:38 +0100

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,59 @@
 import 'phaser';
+// import Background from 'assets/images/background.png';
 
-export default class Demo extends Phaser.Scene {
+export class Scene1 extends Phaser.Scene {
     constructor() {
-        super('demo');
+        super('bootGame');
     }
 
     preload() {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.image("background", "assets/images/background.png");
+        this.load.image("ship", "assets/images/ship.png");
+        this.load.image("ship2", "assets/images/ship2.png");
+        this.load.image("ship3", "assets/images/ship3.png");
     }
 
     create() {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame")
+    }
+}
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+export class Scene2 extends Phaser.Scene {
+    protected background!: Phaser.GameObjects.Image;
+    protected ship!: Phaser.GameObjects.Image;
+    protected ship2!: Phaser.GameObjects.Image;
+    protected ship3!: Phaser.GameObjects.Image;
 
-        this.add.image(400, 300, 'libs');
+    constructor() {
+        super('playGame');
+    }
 
-        const logo = this.add.image(400, 70, 'logo');
+    preload() {
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    }
+
+    create() {
+        this.background = this.add.image(0, 0, "background");
+        this.background.setOrigin(0, 0);
+
+        this.ship = this.add.image(config.width / 2 - 50, config.height / 2, "ship");
+        this.ship2 = this.add.image(config.width / 2, config.height / 2, "ship2");
+        this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, "ship3");
+
+        this.add.text(20, 20, "Playing game", {
+            font: "25px Arial",
+            fill: "yellow"
+        });
     }
 }
 
 const config = {
     type: Phaser.AUTO,
     backgroundColor: '#125555',
-    width: 800,
-    height: 600,
-    scene: Demo
+    width: 256,
+    height: 272,
+    scene: [Scene1, Scene2]
 };
 
 const game = new Phaser.Game(config);
